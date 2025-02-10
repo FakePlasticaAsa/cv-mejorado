2ec36685779206d6a46dbbfb267578ed8901e0ef
FakePlasticAsa <sergibeltranbejar@gmail.com>
2025-02-10 10:29:06 +0100

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import CVpreview from './components/CVpreview';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>CV Preview</h1>
+      <CVpreview />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CVpreview.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import jsPDF from 'jspdf';
+
+function CVpreview() {
+  const generatePDF = () => {
+    const doc = new jsPDF();
+    doc.text('Curriculum Vitae', 10, 10);
+    doc.save('cv.pdf');
+  };
+
+  return (
+    <div>
+      <h2>CV Preview</h2>
+      <button onClick={generatePDF}>Download PDF</button>
+    </div>
+  );
+}
+
+export default CVpreview;
